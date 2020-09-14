80d429201b9553fb3c11ed74b5c5ec3d4538b52b
BFlorian91 <b.florian91@icloud.com>
2020-09-14 14:26:09 +0200

    Initial Commit

---
+++ riseOfKnowledges/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  riseOfKnowledges
+//
+//  Created by Florian Beaumont on 9/14/20.
+//  Copyright © 2020 Florian Beaumont. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

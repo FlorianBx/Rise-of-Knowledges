b587f81908916b2e67419659c6a4de227a5650f3
BFlorian91 <b.florian91@icloud.com>
2020-09-15 01:33:16 +0200

    Feature: start launch page

--- riseOfKnowledges/ContentView.swift
+++ riseOfKnowledges/ContentView.swift
@@ -9,9 +9,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var username = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .center, spacing: 15) {
+            Image("rok")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .padding(.bottom, 50)
+                .padding(.top, 100)
+            
+            TextField("Username", text: self.$username)
+                .padding()
+                .background(Color.gray)
+                .cornerRadius(20.0)
+//                .frame(width: 300, height: 50)
+//            Text(username)
+            
+            Button(action: {}) {
+                Text("Play")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(LinearGradient(gradient: Gradient(colors: [.pink, .purple]), startPoint: .leading, endPoint: .trailing))
+                    .opacity(70.0)
+                    .cornerRadius(20)
+            }
+            Spacer()
+        }
+        .background(Color(red: 0, green: 0, blue: 0.3, opacity: 0.9))
+        .edgesIgnoringSafeArea(.all)
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {

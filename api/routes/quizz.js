f9049d3e1d6044517860cad213d723483df2dd68
Thibault Beaumont <thibault@thibaults-imac.lan>
2020-10-16 19:19:23 +0200

    adding some new stuff

--- api/routes/quizz.js
+++ api/routes/quizz.js
@@ -1,8 +1,8 @@
 import Quizz from '../modele/Quizz';
 
-function getRandomArbitrary(min, max) {
+const getRandomArbitrary = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
-  }
+}
 
 const getQuizz = async (req, res) => {
     let {lang} = req.params
@@ -17,25 +17,19 @@ const getQuizz = async (req, res) => {
     const quizz = await queryHelper.map(async (value, index) => {
         let query = {
             difficulty: value.difficulty,
+            rand: {$gte : Math.random()},
             lang: lang
         }
-        let getQuestion = await Quizz.find(query)
-        let getQuestionLength = getQuestion.length;
+        let getQuestionRandomlyOne = await Quizz.find(query);
+        let getQuestionLength = getQuestionRandomlyOne.length;
         let getRandom = getRandomArbitrary(0, getQuestionLength);
-        return getRandom > value.qty ? getQuestion.slice(getRandom - value.qty, getRandom):
-        getQuestion.slice(getRandom, getRandom + value.qty);
+        return getRandom >= value.qty ? getQuestionRandomlyOne.slice(getRandom - value.qty, getRandom):
+        getQuestionRandomlyOne.slice(getRandom, getRandom + value.qty);
     });
     Promise.all(quizz).then(async resolve => {
         return res.status(200).json(resolve.flat());
     })
     .catch(error => { return res.status(500).json({error: 'no quizz available ' + error }) });
-}
-
-const postResponse = async (req, res) => {
-    const {lang, username, userData} = req.body;
-    // id difficulty et result
-    
-
 }
 
 module.exports = getQuizz;
